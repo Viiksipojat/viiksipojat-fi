56c426a5c4393cf10f3c5572e09d3f40bb96c3a8
Mikko Leino <mikko@viiksipojat.fi>
2013-04-11 21:13:56 +0300

    microscopic amount of added security, see the diff and lolll

--- cgi-bin/github-hook.sh
+++ cgi-bin/github-hook.sh
@@ -11,7 +11,7 @@ LOGFILE=/home/viiksipo/github-hook.log
 REMOTE=$(dig -x $REMOTE_ADDR +short)
 echo $(date -u +'%Y-%m-%d %H:%M:%S') $REMOTE >> $LOGFILE
 
-if [ "${REMOTE:${#REMOTE}-11}" = "github.com." ]; then
+if [ "${REMOTE:${#REMOTE}-12}" = ".github.com." ]; then
 	/home/viiksipo/github/checkout.sh
 fi
 
